679f94257c5c0739b39cf0f0217575dd7ccec033
Emma Hendrick <ehendrick@bgcrv.com>
2024-05-15 15:49:12 -0700

    I realized that thing i did that i thought was smart just broke everything lol

--- hotkeys.py
+++ hotkeys.py
@@ -239,12 +239,9 @@ def do_setup():
 
 # Setup a user
 def do_setup_user():
-    if is_admin():
-        output = setup_user(True)
-        if output is None: return None
-        print_window(output)
-    else:
-        print_window("You need to run this as an admin to setup users.")
+    output = setup_user(True)
+    if output is None: return None
+    print_window(output)
 
 # When we are executed directly
 def main():
